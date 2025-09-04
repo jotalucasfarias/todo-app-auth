d0dde0ae26c1242cad13afdb42ef52e811b9e74e
João Lucas <joaolucasfarias0203@gmail.com>
2025-09-04 00:34:38 -0400

    feat: Adicionar routers e Home Page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./home.css";
+
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    if(email !== '' && password !== ''){
+      alert("TESTE");
+    } else{
+      alert("Preencha todos os campos!");
+    }
+  }
+
+  return (
+    <div className="home-container">
+      <h1>Lista de Tarefas</h1>
+      <span>Gerencie sua agenda de forma fácil.</span>
+
+      <form className="form" onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Digite sua email..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          autoComplete={false}
+          type="password"
+          placeholder="Digite sua senha..."
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Acessar</button>
+      </form>
+
+      <Link className="button-link" to="/register">
+        Não tem uma conta? Cadastre-se
+      </Link>
+    </div>
+  );
+}
---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/home";
+import Register from "../pages/Register";
+
+
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
+}
